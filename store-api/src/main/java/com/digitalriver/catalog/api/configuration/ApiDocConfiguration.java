993a81ca7028c99ba41ebb6782197c7bd865b8ad
Ken Chen <kchen@digitalriver.com>
2014-10-23 19:36:51 +0800

    Added Swagger API Documetation supports.

---
+++ store-api/src/main/java/com/digitalriver/catalog/api/configuration/ApiDocConfiguration.java
@@ -0,0 +1,39 @@
+package com.digitalriver.catalog.api.configuration;
+
+import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.plugin.EnableSwagger;
+import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
+import com.wordnik.swagger.model.ApiInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * ApiDocConfiguration
+ *
+ * @author <a href="mailto:kchen@digitalriver.com">Ken Chen</a>
+ * @since 2014.10
+ */
+@Configuration
+@EnableSwagger
+public class ApiDocConfiguration {
+
+    @Autowired
+    protected SpringSwaggerConfig springSwaggerConfig;
+
+    @Bean
+    public SwaggerSpringMvcPlugin customImplementation(){
+        return new SwaggerSpringMvcPlugin(this.springSwaggerConfig)
+                       .apiInfo(new ApiInfo("Product Store API",
+                                            "The API of product info querying",
+                                            "https://gc.digitalriver.com/store/defaults/en_US/DisplayDRTermsAndConditionsPage/eCommerceProvider.Digital+River,+Inc.",
+                                            "service@digitalriver.com",
+                                            "Digital River",
+                                            "https://gc.digitalriver.com/store/defaults/en_US/DisplayDRTermsAndConditionsPage/eCommerceProvider.Digital+River,+Inc."))
+                       .apiVersion("0.0.1")
+                       .includePatterns(".*pd.*",
+                                        ".*health.*");
+    }
+
+
+}
